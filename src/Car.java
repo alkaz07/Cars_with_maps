b92453a6230f05d7d5501fd0913ae99acb6574f9
alkaz07 <>
2023-08-11 11:50:30 +0300

    +exemple2, 3

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,6 @@
-class Car {
+import java.util.Objects;
+
+class Car implements Comparable<Car> {
     String name;
     int speed;
     int gruz;
@@ -10,6 +12,36 @@ class Car {
     public String toString()
     {
         return name+"_"+speed+"_"+gruz;
+    }
+
+    //Машинки считаются одинаковыми, если:
+    //совпадает name И совпадет speed И совпадает gruz
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return speed == car.speed && gruz == car.gruz && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, speed, gruz);
+    }
+
+
+    @Override
+    public int compareTo(Car o) {
+        if(this.gruz == o.gruz)
+        {
+            if (this.speed == o.speed)
+                return this.name.compareTo(o.name);
+            else
+                return this.speed-o.speed;
+        }
+        else
+            return this.gruz-o.gruz;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,101 @@
 //заполнив туда значения из уже имеющегося ArrayList
 //вывести его
 
-import java.util.ArrayList;
-
-import java.util.Comparator;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Car> cars = new ArrayList<>();
-        cars.add(new Car("ЗиЛ",      60, 2000));
-        cars.add(new Car("Форд",    200, 900));
-        cars.add(new Car("Феррари", 300, 500));
-        cars.add(new Car("Лада",    160, 1500));
-        cars.add(new Car("Форд",    200, 999));
-        cars.add(new Car("Феррари", 330, 500));
-        cars.add(new Car("Лада",    160, 1500));
-        System.out.println(cars);
-        cars.sort(Comparator.comparingInt(x -> x.speed));
-        System.out.println(cars);
-        cars.sort(Comparator.comparing(q->q.name));
-        System.out.println(cars);
-      
+        //exemple1();
+        exemple2();
+        exemple3();
+    }
+
+    private static void exemple3() {
+        //прочитать исходный файл и вычислить, сколько одинаковых машин каждого типа.
+        //отсортировать по грузоподъемности
+        //SELECT car.name, car.speed, car.gruz, count(*) FROM cars GROUP BY car.name, car.speed, car.gruz ORDER BY gruz, speed, name;
+        TreeMap<Car, Integer> map1 = new TreeMap<>();
+        try {
+            Scanner scanner = new Scanner(new File("input.txt"));
+            while (scanner.hasNextLine()) {
+                String[] words = scanner.nextLine().split(";");
+                Car car = new Car(words[0], Integer.parseInt(words[1]), Integer.parseInt(words[2]));
+                if(!map1.containsKey(car))
+                    map1.put(car, 1);
+                else
+                    map1.put(car, map1.get(car)+1);
+            }
+            scanner.close();
+            System.out.println(map1);
+
+        } catch (IOException e) {
+            System.out.println("не удалось найти файл");
+        }
+
+    }
+
+    private static void exemple2() {
+        //прочитать исходный файл и вычислить, сколько одинаковых машин каждого типа
+        //SELECT car.name, car.speed, car.gruz, count(*) from cars group by car.name, car.speed, car.gruz;
+
+        //составим словарь Машина -> число
+        HashMap<Car, Integer> map1 = new HashMap<>();
+        try {
+            Scanner scanner = new Scanner(new File("input.txt"));
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] words = line.split(";");
+                Car car = new Car(words[0], Integer.parseInt(words[1]), Integer.parseInt(words[2]));
+                if(!map1.containsKey(car))
+                    map1.put(car, 1);
+                else
+                    map1.put(car, map1.get(car)+1);
+                //map1.put(car, (map1.get(car) == null) ? 1 : map1.get(car) + 1);
+            }
+            scanner.close();
+            System.out.println(map1);
+
+        } catch (IOException e) {
+            System.out.println("не удалось найти файл");
+        }
+    }
+
+    private static void exemple1() {
+        HashMap<String, Car> mapFIOCar = new HashMap<>();
+        mapFIOCar.put("Вася", new Car("ЗиЛ", 60, 2000));
+        mapFIOCar.put("Маша", new Car("Форд", 200, 900));
+        mapFIOCar.put("Гриша", new Car("Феррари", 300, 500));
+        mapFIOCar.put("Коля", new Car("Лада", 160, 1500));
+        mapFIOCar.put("Миша", new Car("Форд", 200, 999));
+        mapFIOCar.put("Нина", new Car("Феррари", 330, 500));
+        System.out.println(mapFIOCar);
+        mapFIOCar.put("Гриша", new Car("Лада", 160, 1500));
+        System.out.println(mapFIOCar);
+
+        System.out.println("введите имя");
+        Scanner scanner = new Scanner(System.in);
+        String fio = scanner.nextLine();
+        if (mapFIOCar.containsKey(fio))
+            System.out.println(fio + " ездит на " + mapFIOCar.get(fio));
+        else
+            System.out.println(fio + " - пешеход");
+        for (Map.Entry<String, Car> currentLine : mapFIOCar.entrySet()) {
+            System.out.println(currentLine);
+            System.out.println(currentLine.getKey() + " ::: " + currentLine.getValue());
+        }
+
+        System.out.println("в словаре такие фамилии: " + mapFIOCar.keySet());
+        System.out.println("в словаре такие машинки: " + mapFIOCar.values());
+        ArrayList<Car> cars = new ArrayList<>(mapFIOCar.values());
+        cars.sort(Comparator.comparing(x -> x.name));
+        for (Car c : cars) {
+            System.out.println(c + "\tпод управлением ???");
+            for (Map.Entry<String, Car> entr : mapFIOCar.entrySet()) {
+                if (entr.getValue().equals(c))
+                    System.out.println("\t" + entr.getKey());
+            }
+        }
     }
 }
